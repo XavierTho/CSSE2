1020f6a58a2af30a1d18c486ea30415e9363c616
XavierTho <xavierathompson@gmail.com>
2023-12-05 12:52:42 -0800

    removed killGoomba code

--- assets/js/platformer/Enemy.js
+++ assets/js/platformer/Enemy.js
@@ -30,21 +30,6 @@ export class Enemy extends Character {
         //Initially get the enemy moving
         this.x += this.speed;
     }
-
-    
-    killGoomba() {
-        let currentScale = 1;
-        let intervalId = setInterval(() => {
-            if (currentScale >= 0) {
-                canvas.style.transform = `scale(1, ${currentScale.toFixed(1)})`;
-                console.log("Death " + currentScale.toFixed(1));
-                currentScale -= 0.1;
-            } else {
-                clearInterval(intervalId);
-            }
-        }, 10);
-        super.destroy();
-    }
 }
 
 export default Enemy
