821a7a35cbdd2b48abae4a014fca238c38dbeb65
JustinQ-DNHS <justin.q224@icloud.com>
2023-12-04 23:14:59 -0800

    failed changes

--- assets/js/platformer/Enemy.js
+++ assets/js/platformer/Enemy.js
@@ -28,6 +28,29 @@ export class Enemy extends Character {
         super.destroy();
     }
 }
+// This code did not work, it was used to attempt to move the original position of the goomba
+// Please refer to the character.js to change spawn location. You guy's got this.
+/* size() {
+    // Formula for Height should be on constant ratio, using a proportion of 832
+    const scaledHeight = GameEnv.innerHeight * (100 / 832);
+    // Formula for Width is scaled: scaledWidth/scaledHeight == this.width/this.height
+    const scaledWidth = scaledHeight * this.aspect_ratio;
+    const enemyX = .60 * GameEnv.innerWidth;
+    const enemyY = (GameEnv.bottom - scaledHeight);
 
+    // set variables used in Display and Collision algorithms
+    this.bottom = enemyY;
+    this.collisionHeight = scaledHeight;
+    this.collisionWidth = scaledWidth;
+
+    //this.canvas.width = this.width; 
+    //this.canvas.height = this.height;
+    this.canvas.style.width = `${scaledWidth}px`;
+    this.canvas.style.height = `${scaledHeight}px`;
+    this.canvas.style.position = 'absolute';
+    this.canvas.style.left = `${enemyX}px`;
+    this.canvas.style.top = `${enemyY}px`; 
+
+} */
 
 export default Enemy
